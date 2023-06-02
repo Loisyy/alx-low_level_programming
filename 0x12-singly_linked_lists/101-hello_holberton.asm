954d071671049000eb2f345c77a82dab87458c0e
Loisyy <nkeiruloisy@yahoo.com>
2023-06-02 14:08:59 -0700

    task 0

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,10 @@
-global  main
-    message db "Hello, Holberton", 0
-    format db "%s", 0
-
-Global main
-    extern printf
-
-global _start
-_start:
-    sub rsp, 8
-
-    mov rdi, format
-    mov rsi, message
-    xor eax, eax
-    call printf
-
-    add rsp, 8
-
-    mov eax, 60
-    xor edi, edi
+       global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+ format db "%s", 0
+format: db `Hello, Holberton\n`,0
