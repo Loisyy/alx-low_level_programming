6d2ca8c440de4441bfabde2acdfd9966d2dd356a
Loisyy <nkeiruloisy@yahoo.com>
2023-06-13 11:17:49 -0700

    task to date

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * Description:create_file - Creates a file.
+ * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
+ *
  * Return: If the function fails - -1.
- * Otherwise - 1.
+ *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -16,11 +17,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];len++;)
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fl, text_content, len);
+	w = write(fd, text_content, len);
 
 	if (fd == -1 || w == -1)
 		return (-1);
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * Description:append_text_to_file - Appends text at the end of a file.
+ * append_text_to_file - Appends text at the end of a file.
  * @filename: A pointer to the name of the file.
  * @text_content: The string to add to the end of the file.
+ *
  * Return: If the function fails or filename is NULL - -1.
- * If the file does not exist the user lacks write permissions - -1.
- * Otherwise - 1.
+ *         If the file does not exist the user lacks write permissions - -1.
+ *         Otherwise - 1.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
@@ -17,7 +18,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];len++;)
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
 	o = open(filename, O_WRONLY | O_APPEND);
