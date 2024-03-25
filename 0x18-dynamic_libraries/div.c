1458ed0041684830ab34331d373adbeb4377442d
Loisyy <nkeiruloisy@yahoo.com>
2024-03-25 15:23:28 -0700

    using shared library on python thta contains c functions

---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * my_div - divide two integers
+ * @a: first int
+ * @b: second int
+ * Return: division result 
+ */
+int my_div(int a, int b)
+{
+    return (a / b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * subtract - subtract two integers
+ * @a: first int
+ * @b second int
+ *return: subtracted 
+ */
+int subtract(int a, int b)
+{
+	return (a - b);
+}	
