27b6f1c8beb1a6d22066f70c153de37b26248614
Loisyy <nkeiruloisy@yahoo.com>
2024-06-04 04:51:56 -0700

    0x1C-makefile 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,27 +1,25 @@
+#!/usr/bin/python3
+"""Module to define a function island_perimeter"""
+
+
 def island_perimeter(grid):
-    """
-    Function to calculate the perimeter of an island described in a grid.
+    """Function to calculate an island described in a grid perimeter
     grid is a list of list of integers:
     0 represents a water zone
     1 represents a land zone
-    One cell is a square with side length 1.
+    One cell is a square with side length 1
     """
     width = len(grid[0])
     height = len(grid)
     edges = 0
     size = 0
 
-    for i in range(height):
-        for j in range(width):
-            if grid[i][j] == 1:
+    for a in range(height):
+        for b in range(width):
+            if grid[a][b] == 1:
                 size += 1
-                # Check left neighbor
-                if j > 0 and grid[i][j - 1] == 1:
+                if (b > 0 and grid[a][b - 1] == 1):
                     edges += 1
-                # Check top neighbor
-                if i > 0 and grid[i - 1][j] == 1:
+                if (a > 0 and grid[a - 1][b] == 1):
                     edges += 1
-
-    # Each cell adds 4 to the perimeter, each shared edge subtracts 2
     return size * 4 - edges * 2
-
